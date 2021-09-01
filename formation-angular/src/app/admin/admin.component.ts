87bca6e960bfc1d678cc1840c7c5972efa66d98b
Florian F <favetta.florian@gmail.com>
2021-09-01 22:28:07 +0200

    Admin(User)

--- formation-angular/src/app/admin/admin.component.ts
+++ formation-angular/src/app/admin/admin.component.ts
@@ -9,6 +9,7 @@ import {MonstreHttpService} from "../monstre-Http.service";
 import {MarchandHttpService} from "../marchand/marchand-http.service";
 import {UtilisateurHttpService} from "../utilisateur-http.service";
 import {Arme} from "../model/Arme";
+import {Utilisateur} from "../model/utilisateur";
 
 @Component({
   selector: 'app-admin',
@@ -20,6 +21,7 @@ export class AdminComponent implements OnInit {
 
   adminForm: Admin = null;
   armeForm: Arme = null;
+  userForm: Utilisateur = null;
 
   constructor(private adminService: AdminHttpService, private armeService: ArmeHttpService, private armureService: ArmureHttpService, private potionService: PotionHttpService,
               private heroService: HeroHttpService, private monstreService: MonstreHttpService, private marchandService: MarchandHttpService, private utilisateurService: UtilisateurHttpService) {
@@ -31,14 +33,25 @@ export class AdminComponent implements OnInit {
   list(): Array<Admin> {
     return this.adminService.findAll();
   }
+  listUser(): Array<Utilisateur> {
+    return this.utilisateurService.findAll();
+  }
 
   add() {
     this.adminForm = new Admin();
+  }
+  addUser() {
+    this.userForm = new Utilisateur();
   }
 
   edit(id: number) {
     this.adminService.findById(id).subscribe(resp => {
       this.adminForm = resp;
+    })
+  }
+  editUser(id: number) {
+    this.utilisateurService.findById(id).subscribe(resp => {
+      this.userForm = resp;
     })
   }
 
@@ -50,15 +63,31 @@ export class AdminComponent implements OnInit {
     }
     this.adminForm = null;
   }
+  saveUser() {
+    if (this.userForm.id) {
+      this.utilisateurService.modify(this.userForm);
+    } else {
+      this.utilisateurService.create(this.userForm);
+    }
+    this.userForm = null;
+  }
 
   delete(id: number){
     this.adminService.deleteById(id).subscribe(resp => {
       this.adminService.load();
     }, error => console.log(error));
   }
+  deleteUser(id: number) {
+    this.utilisateurService.deleteById(id).subscribe(resp => {
+      this.utilisateurService.load();
+    }, error => console.log(error));
+  }
 
   cancel() {
     this.adminForm = null;
+  }
+  cancelUser() {
+    this.userForm = null;
   }
 
   loading(){
