d6a9f3dcc1fcca9304a199e6b9edc23a11b75ab3
Florian F <favetta.florian@gmail.com>
2021-09-01 14:38:27 +0200

    Insc

--- formation-angular/src/app/inscription/inscription.component.ts
+++ formation-angular/src/app/inscription/inscription.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UtilisateurHttpService} from "../utilisateur-http.service";
+import {Utilisateur} from "../model/utilisateur";
 
 @Component({
   selector: 'app-inscription',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InscriptionComponent implements OnInit {
 
-  constructor() { }
+  inscForm: Utilisateur = null;
+
+  constructor(private utilisateurService: UtilisateurHttpService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  add() {
+    this.inscForm = new Utilisateur();
+  }
+
+  save() {
+
+    this.utilisateurService.create(this.inscForm);
+
+    this.inscForm = null;
   }
 
 }
