6e4ddd04191b2f3f2fcf326a7ce7d34a2d154582
Xavier <xavier.darcissac@gmail.com>
2021-09-02 09:27:24 +0200

    ,

--- formation-angular/src/app/app.module.ts
+++ formation-angular/src/app/app.module.ts
@@ -52,7 +52,7 @@ import {CommonModule} from "@angular/common";
     ArmureComponent,
     ArmeComponent,
     AdminComponent,
-    ConnexionComponent
+    ConnexionComponent,
     InscriptionComponent,
     ObjetComponent
   ],
